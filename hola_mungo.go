47a5762e15d206511759e0b9a6346d14ae8bb3aa
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 13:52:43 -0500

    Variables básicas

--- hola_mungo.go
+++ hola_mungo.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
-	// "time"
 )
 
 func main() {
-	// time.Sleep(time.Second * 5)
+	var suma = 8 + 9
+	var resta int = 6 - 4
+	var nombre string = "Carlos Espinoza"
+
 	fmt.Println("Hola Mundo desde Go con Charlie Espinoza")
+	fmt.Println(suma)
+	fmt.Println(resta)
+	fmt.Println(nombre)
 }
