746d8208fdc9829ea1b0c5a366de06863fb431cb
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 14:16:18 -0500

    Struct

--- hola_mungo.go
+++ hola_mungo.go
@@ -4,9 +4,27 @@ import (
 	"fmt"
 )
 
+type Gorra struct {
+	marca  string
+	color  string
+	precio float32
+	plana  bool
+}
+
 func main() {
+	var gorra_negra = Gorra{
+		marca:  "Nike",
+		color:  "Negro",
+		precio: 25.50,
+		plana:  false}
+
+	var gorra_roja = Gorra{"Adidas", "Roja", 25.50, false}
+
 	var numero1 = 10.0
 	var numero2 = 6.0
+
+	fmt.Println(gorra_negra)
+	fmt.Println(gorra_roja)
 
 	fmt.Print("La suma es: ")
 	fmt.Println(numero1 + numero2)
