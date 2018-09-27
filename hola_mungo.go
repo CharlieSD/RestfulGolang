2effbb987a7e64d7bceac2ad715429feaf85ad23
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 14:01:36 -0500

    Variables

--- hola_mungo.go
+++ hola_mungo.go
@@ -7,8 +7,12 @@ import (
 func main() {
 	var suma = 8 + 9
 	var resta int = 6 - 4
-	var nombre string = "Carlos Espinoza"
-	fmt.Println("Hola Mundo desde Go con Charlie Espinoza")
+	var nombre string = "Carlos "
+	var apellido string = "Espinoza" // Declaración tipada
+
+	pais := "México" // Asignación inferida
+
+	fmt.Println("Hola Mundo desde Go con " + nombre + apellido + " desde " + pais)
 	fmt.Println(suma)
 	fmt.Println(resta)
 	fmt.Println(nombre)
@@ -18,4 +22,5 @@ func main() {
 	En terminal comando:
 	- godoc: muestra la documentación de alguna biblioteca
 	- go fmt [fichero]: da correcto formato del archivo
+	- go build [fichero]: crea el compilado
 */
