a13ea103d0bff2797460a3753b44ae146ccbe29f
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 13:57:15 -0500

    Comandos comunes

--- hola_mungo.go
+++ hola_mungo.go
@@ -8,9 +8,14 @@ func main() {
 	var suma = 8 + 9
 	var resta int = 6 - 4
 	var nombre string = "Carlos Espinoza"
-
 	fmt.Println("Hola Mundo desde Go con Charlie Espinoza")
 	fmt.Println(suma)
 	fmt.Println(resta)
 	fmt.Println(nombre)
 }
+
+/*
+	En terminal comando:
+	- godoc: muestra la documentación de alguna biblioteca
+	- go fmt [fichero]: da correcto formato del archivo
+*/
