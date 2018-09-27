f787aec01f83f4e333ba4ffd755a4f0eb62c8218
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 16:59:44 -0500

    Escritura de archivos

--- lector.go
+++ lector.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 	fmt.Println("Lectura de fichero")
 
-	text, err := ioutil.ReadFile("fichero.txt")
+	nuevoTexto := os.Args[1] + "\n"
+
+	//nuevoFichero := ioutil.WriteFile("elotrofichero.txt", nuevoTexto, 0777)
+
+	fichero, err := os.OpenFile("elotrofichero.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	showError(err)
+
+	tamaño, err := fichero.WriteString(nuevoTexto)
+	fmt.Println(tamaño)
+	showError(err)
+
+	fichero.Close()
+
+	text, err := ioutil.ReadFile("elotrofichero.txt")
 	showError(err)
 	fmt.Println(string(text))
 }
