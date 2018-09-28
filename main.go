187320fd1194bcef85da5eabd5edbbf23848286b
CharlieSD <Carlos.espinoza@debite.com.mx>
2018-09-27 21:55:27 -0500

    Parametros URL

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/contacto", Contact)
+	router.HandleFunc("/peliculas", MovieList)
+	router.HandleFunc("/peliculas/{id}", MovieShow)
 
 	server := http.ListenAndServe(":8080", router)
 
@@ -23,7 +24,15 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hola mundo desde mi servidor Web em Go")
 }
 
-// Contact ...
-func Contact(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Esta es la pagina de contacto")
+// MovieList ...
+func MovieList(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Esta es la pagina de Películas")
+}
+
+// MovieShow ...
+func MovieShow(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movieID := params["id"]
+
+	fmt.Fprintf(w, "Has cargado la pelicula numero %s", movieID)
 }
